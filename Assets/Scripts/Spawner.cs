8ebf51df70a76c409ed66f8ed1e57b15e325ce69
Jaeseok <jaeseok1111@gmail.com>
2022-10-25 16:09:32 +0900

    장애물 생성

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    private float spawnTime = 1f;
+    private float startSpawnTime = 1f;
+    [SerializeField]
+    private float endSpawnTime = 3f;
     [SerializeField]
     private GameObject[] spawnList;
 
     private float time = 0f;
+    private float spawnTime = 0f;
+
+    private void Start()
+    {
+        spawnTime = Random.Range(startSpawnTime, endSpawnTime);
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,6 +29,7 @@ public class Spawner : MonoBehaviour
             Instantiate(spawnPrefab, transform.position, transform.rotation, transform);
 
             time = 0f;
+            spawnTime = Random.Range(startSpawnTime, endSpawnTime);
         }
     }
 }
